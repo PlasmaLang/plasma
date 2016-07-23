ceda01a268da90c0ab75aa61d29d4b71a78c0334
Paul Bone <paul@bone.id.au>
2016-07-24 00:03:34 +1000

    [refactor] Fixup file haaders.
    Fix copyright notices and one description.
    
    src/ast.env.m:
    src/ast.m:
    src/ast.nonlocals.m:
    src/ast.resolve.m:
        As above.

--- src/ast.resolve.m
+++ src/ast.resolve.m
@@ -2,7 +2,7 @@
 % Plasma AST symbol resolution
 % vim: ts=4 sw=4 et
 %
-% Copyright (C) 2015 Plasma Team
+% Copyright (C) 2015-2016 Plasma Team
 % Distributed under the terms of the MIT License see ../LICENSE.code
 %
 % This module resolves symbols within the Plasma AST.
