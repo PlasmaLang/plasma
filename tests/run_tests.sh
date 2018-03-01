c4bba041e0f8a4d7655201f788cbede814ac17bd
Paul Bone <paul@bone.id.au>
2018-03-02 00:55:22 +1100

    [tests] Output colours properly in the test script
    tests/run_tests.sh:
        Thanks CcxWrk.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -6,8 +6,6 @@
 # vim: noet sw=4 ts=4
 #
 
-# TODO: Use portable colours or don't use colours when not supported.
-
 set -e
 
 NUM_TESTS=0
@@ -15,6 +13,12 @@ NUM_SUCCESSES=0
 FAILURE=0
 TESTS=""
 FAILING_TESTS=""
+
+if [ 8 -le $(tput colors) ]; then
+    TTY_TEST_SUCC=$(tput setaf 2)
+    TTY_TEST_FAIL=$(tput setaf 1)
+    TTY_RST=$(tput op)
+fi
 
 for PZTFILE in pzt/*.pzt; do
     TESTS="$TESTS ${PZTFILE%.pzt}"
@@ -32,10 +36,10 @@ for TEST in $TESTS; do
     # Wrapping this up in a test and negating it is a bit annoying, but it
     # was the easy way I could redirect the output and errors successfully.
     if [ ! "$(cd $DIR ; make $NAME.test 2>&1 > $NAME.log)" ]; then
-        echo -n "\033[1;32m.\033[0m"
+        printf '%s.%s' "$TTY_TEST_SUCC" "$TTY_RST"
         NUM_SUCCESSES=$(($NUM_SUCCESSES + 1))
     else
-        echo -n "\033[1;31m*\033[0m"
+        printf '%s*%s' "$TTY_TEST_FAIL" "$TTY_RST"
         FAILURE=1
         FAILING_TESTS="$FAILING_TESTS $TEST"
     fi
@@ -44,13 +48,13 @@ done
 printf '\n'
 
 if [ $FAILURE -eq 0 ]; then
-    echo "\033[1;32mAll $NUM_TESTS tests passed\033[0m"
+    printf '%sAll %d tests passed %s\n' "$TTY_TEST_SUCC" "$NUM_TESTS" "$TTY_RST"
 else
     NUM_FAILED=$(( $NUM_TESTS - $NUM_SUCCESSES ))
-    echo -n "$NUM_SUCCESSES out of $NUM_TESTS passed, "
-    echo "\033[1;31m$NUM_FAILED failed\033[0m"
+    printf '%d out of %d passed, ' "$NUM_SUCCESSES" "$NUM_TESTS"
+    printf '%s%d failed%s\n' "$TTY_TEST_FAIL" "$NUM_FAILED" "$TTY_RST"
 
-    echo "Failing tests: $FAILING_TESTS"
+    printf 'Failing tests: %s\n' "$FAILING_TESTS"
 fi
 
 return $FAILURE
