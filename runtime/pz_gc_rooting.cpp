286c4142c2d65809efcd7164acdcd741085dbd21
Paul Bone <paul@bone.id.au>
2019-01-20 23:50:54 +1100

    [pzrun] Update style: use m_ prefix for data members in GC rooting classes
    runtime/pz_gc_rooting.{h,cpp}:
        As above.

--- runtime/pz_gc_rooting.cpp
+++ runtime/pz_gc_rooting.cpp
@@ -15,21 +15,21 @@ namespace pz {
 
 void Tracer::do_trace(PZ_Heap_Mark_State *state) const
 {
-    for (void *root : roots) {
+    for (void *root : m_roots) {
         pz_gc_mark_root(state, *(void**)root);
     }
 }
 
 void Tracer::add_root(void *root)
 {
-    roots.push_back(root);
+    m_roots.push_back(root);
 }
 
 void Tracer::remove_root(void *root)
 {
-    assert(!roots.empty());
-    assert(roots.back() == root);
-    roots.pop_back();
+    assert(!m_roots.empty());
+    assert(m_roots.back() == root);
+    m_roots.pop_back();
 }
 
 }
