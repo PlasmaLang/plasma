bfeeabe02f4758e511f6c12b584c48600746617c
Paul Bone <paul@bone.id.au>
2019-02-18 22:25:54 +1100

    [plasmac/pz] Reorder items in core_to_pz.locn.m
    Rename the items in this file so that they are grouped once again by
    static/dynamic.
    
    src/core_to_pz.locn.m:
        As above.

--- src/core_to_pz.locn.m
+++ src/core_to_pz.locn.m
@@ -15,17 +15,6 @@
 
 %-----------------------------------------------------------------------%
 
-%
-% The location map information is divided into two halves, the static
-% information which is static per PZ procedure.  And the dyanmic
-% information, which changes with code generation (for example as values are
-% pushed onto the stack).
-%
-
-:- type val_locn_map_static.
-
-:- type var_locn_map.
-
     % The location of a variable.
     %
 :- type var_locn
@@ -43,9 +32,16 @@
     --->    sl_module_env(field_num).
 
 %-----------------------------------------------------------------------%
+%
+% The location map information is divided into two halves, the static
+% information which is static per PZ procedure.  And the dyanmic
+% information, which changes with code generation (for example as values are
+% pushed onto the stack).
+%
 
-:- pred vl_start_var_binding(val_locn_map_static::in, var_locn_map::out)
-    is det.
+:- type val_locn_map_static.
+
+:- func sl_init = val_locn_map_static.
 
 :- pred vl_set_proc(func_id::in, pzp_id::in,
     val_locn_map_static::in, val_locn_map_static::out) is det.
@@ -56,6 +52,21 @@
 :- pred vl_set_proc_imported(func_id::in, pzi_id::in, field_num::in,
     val_locn_map_static::in, val_locn_map_static::out) is det.
 
+:- func vl_lookup_proc_id(val_locn_map_static, func_id) = pzp_id.
+
+:- pred sl_search(val_locn_map_static::in, string::in, str_locn::out)
+    is semidet.
+
+:- pred sl_insert(string::in, field_num::in, val_locn_map_static::in,
+    val_locn_map_static::out) is det.
+
+%-----------------------------------------------------------------------%
+
+:- type var_locn_map.
+
+:- pred vl_start_var_binding(val_locn_map_static::in, var_locn_map::out)
+    is det.
+
 :- pred vl_put_var(var::in, int::in, var_locn_map::in, var_locn_map::out)
     is det.
 
@@ -64,21 +75,9 @@
 
 :- func vl_lookup_proc(var_locn_map, func_id) = proc_locn.
 
-:- func vl_lookup_proc_id(val_locn_map_static, func_id) = pzp_id.
-
 :- func vl_lookup_var(var_locn_map, var) = var_locn.
 
-%-----------------------------------------------------------------------%
-
-:- func sl_init = val_locn_map_static.
-
 :- func sl_lookup(var_locn_map, string) = str_locn.
-
-:- pred sl_search(val_locn_map_static::in, string::in, str_locn::out)
-    is semidet.
-
-:- pred sl_insert(string::in, field_num::in, val_locn_map_static::in,
-    val_locn_map_static::out) is det.
 
 %-----------------------------------------------------------------------%
 %-----------------------------------------------------------------------%
@@ -94,15 +93,9 @@
                 vls_proc_id_map         :: map(func_id, proc_locn)
             ).
 
-:- type var_locn_map
-    --->    var_locn_map(
-                vl_static               :: val_locn_map_static,
-                vl_vars                 :: map(var, var_locn)
-            ).
-
 %-----------------------------------------------------------------------%
 
-vl_start_var_binding(Static, var_locn_map(Static, map.init)).
+sl_init = val_locn_map_static(init, init).
 
 %-----------------------------------------------------------------------%
 
@@ -121,6 +114,43 @@ vl_set_proc_imported(FuncId, ImportId, FieldNum, !Map) :-
 vl_set_proc_1(FuncId, Locn, !Map) :-
     map.det_insert(FuncId, Locn, !.Map ^ vls_proc_id_map, ProcMap),
     !Map ^ vls_proc_id_map := ProcMap.
+
+%-----------------------------------------------------------------------%
+
+vl_lookup_proc_id(Map, FuncId) = ProcId :-
+    map.lookup(Map ^ vls_proc_id_map, FuncId, Locn),
+    ( Locn = pl_static_proc(ProcId)
+    ;
+        ( Locn = pl_instrs(_)
+        ; Locn = pl_import(_, _)
+        ),
+        unexpected($file, $pred, "Non-static proc")
+    ).
+
+%-----------------------------------------------------------------------%
+
+sl_search(Map, Str, Locn) :-
+    map.search(Map ^ vls_const_data, cd_string(Str), Locn).
+
+%-----------------------------------------------------------------------%
+
+sl_insert(String, FieldNum, !Map) :-
+    map.det_insert(cd_string(String), sl_module_env(FieldNum),
+        !.Map ^ vls_const_data, ConstMap),
+    !Map ^ vls_const_data := ConstMap.
+
+%-----------------------------------------------------------------------%
+%-----------------------------------------------------------------------%
+
+:- type var_locn_map
+    --->    var_locn_map(
+                vl_static               :: val_locn_map_static,
+                vl_vars                 :: map(var, var_locn)
+            ).
+
+%-----------------------------------------------------------------------%
+
+vl_start_var_binding(Static, var_locn_map(Static, map.init)).
 
 %-----------------------------------------------------------------------%
 
@@ -144,42 +174,15 @@ vl_put_vars([Var | Vars], Depth0, Varmap, Comments, !Map) :-
 vl_lookup_proc(Map, FuncId) = Locn :-
     map.lookup(Map ^ vl_static ^ vls_proc_id_map, FuncId, Locn).
 
-vl_lookup_proc_id(Map, FuncId) = ProcId :-
-    map.lookup(Map ^ vls_proc_id_map, FuncId, Locn),
-    ( Locn = pl_static_proc(ProcId)
-    ;
-        ( Locn = pl_instrs(_)
-        ; Locn = pl_import(_, _)
-        ),
-        unexpected($file, $pred, "Non-static proc")
-    ).
-
 %-----------------------------------------------------------------------%
 
 vl_lookup_var(Map, Var) = Locn :-
     map.lookup(Map ^ vl_vars, Var, Locn).
 
 %-----------------------------------------------------------------------%
-%-----------------------------------------------------------------------%
-
-sl_init = val_locn_map_static(init, init).
-
-%-----------------------------------------------------------------------%
 
 sl_lookup(Map, Str) = Locn :-
     map.lookup(Map ^ vl_static ^ vls_const_data, cd_string(Str), Locn).
-
-%-----------------------------------------------------------------------%
-
-sl_search(Map, Str, Locn) :-
-    map.search(Map ^ vls_const_data, cd_string(Str), Locn).
-
-%-----------------------------------------------------------------------%
-
-sl_insert(String, FieldNum, !Map) :-
-    map.det_insert(cd_string(String), sl_module_env(FieldNum),
-        !.Map ^ vls_const_data, ConstMap),
-    !Map ^ vls_const_data := ConstMap.
 
 %-----------------------------------------------------------------------%
 %-----------------------------------------------------------------------%
