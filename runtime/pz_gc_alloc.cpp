3e78e629889a142be905a5caa3b294468779ffce
Paul Bone <paul@bone.id.au>
2019-07-16 23:51:14 +1000

    [rt/gc] Split up more of pz_gc.cpp
    Collection and allocation code into their own C++ files.

---
+++ runtime/pz_gc_alloc.cpp
@@ -0,0 +1,163 @@
+/*
+ * Plasma garbage collector collection procedures 
+ * vim: ts=4 sw=4 et
+ *
+ * Copyright (C) 2019 Plasma Team
+ * Distributed under the terms of the MIT license, see ../LICENSE.code
+ */
+
+#include "pz_common.h"
+
+#include <string.h>
+
+#include "pz_util.h"
+
+#include "pz_gc.h"
+#include "pz_gc_util.h"
+
+#include "pz_gc.impl.h"
+#include "pz_gc_layout.h"
+
+namespace pz {
+
+void *
+Heap::alloc(size_t size_in_words, const GCCapability &gc_cap)
+{
+    assert(size_in_words > 0);
+
+    void *cell;
+#ifdef PZ_DEV
+    if (m_options.gc_zealous() &&
+        gc_cap.can_gc() &&
+        !is_empty())
+    {
+        // Force a collect before each allocation in this mode.
+        cell = NULL;
+    } else
+#endif
+    {
+        cell = try_allocate(size_in_words);
+    }
+    if (cell == NULL && gc_cap.can_gc()) {
+        collect(&gc_cap.tracer());
+        cell = try_allocate(size_in_words);
+        if (cell == NULL) {
+            fprintf(stderr, "Out of memory, tried to allocate %lu bytes.\n",
+                        size_in_words * WORDSIZE_BYTES);
+            abort();
+        }
+    }
+
+    return cell;
+}
+
+void *
+Heap::alloc_bytes(size_t size_in_bytes, const GCCapability &gc_cap) {
+    size_t size_in_words = AlignUp(size_in_bytes, WORDSIZE_BYTES) /
+        WORDSIZE_BYTES;
+
+    return alloc(size_in_words, gc_cap);
+}
+
+void *
+Heap::try_allocate(size_t size_in_words)
+{
+    Cell *cell;
+
+    /*
+     * Try the free list
+     */
+    size_in_words = size_in_words < GC_MIN_CELL_SIZE ? GC_MIN_CELL_SIZE :
+        size_in_words;
+    LBlock *block = get_free_list(size_in_words);
+    if (!block) {
+        block = allocate_block(size_in_words);
+        if (!block) {
+            #ifdef PZ_DEV
+            if (m_options.gc_trace2()) {
+                fprintf(stderr, "Heap full for allocation of %ld words\n",
+                        size_in_words);
+            }
+            #endif
+            return nullptr;
+        }
+    }
+
+    cell = block->allocate_cell();
+
+    if (!cell) return nullptr;
+
+    #ifdef PZ_DEV
+    if (m_options.gc_trace2()) {
+        fprintf(stderr, "Allocated %p from free list\n", cell);
+    }
+    #endif
+
+    return cell;
+}
+
+LBlock *
+Heap::get_free_list(size_t size_in_words)
+{
+    for (unsigned i = 0; i < GC_LBLOCK_PER_BBLOCK; i++) {
+        LBlock *lblock = &(m_bblock->m_blocks[i]);
+
+        // TODO: Appropiriate size?
+        if (lblock->is_in_use() && lblock->size() == size_in_words &&
+                !lblock->is_full())
+        {
+            return lblock;
+        }
+    }
+
+    return nullptr;
+}
+
+LBlock *
+Heap::allocate_block(size_t size_in_words)
+{
+    LBlock *block;
+
+    if (m_bblock->m_wilderness * GC_LBLOCK_SIZE >= m_heap_size)
+        return nullptr;
+
+    block = m_bblock->next_block();
+    if (!block) return nullptr;
+
+    #ifdef PZ_DEV
+    if (m_options.gc_trace()) {
+        fprintf(stderr, "Allocated new block for %ld cells\n", size_in_words);
+    }
+    #endif
+
+    new(block) LBlock(size_in_words);
+
+    return block;
+}
+
+LBlock*
+BBlock::next_block()
+{
+    if (m_wilderness >= GC_LBLOCK_PER_BBLOCK)
+        return nullptr;
+
+    return &m_blocks[m_wilderness++];
+}
+
+Cell*
+LBlock::allocate_cell()
+{
+    assert(is_in_use());
+
+    for (unsigned i = 0; i < num_cells(); i++) {
+        if (0 == (*cell_bits(i) & GC_BITS_ALLOCATED)) {
+            assert(*cell_bits(i) == 0);
+            *cell_bits(i) = GC_BITS_ALLOCATED;
+            return cell(i);
+        }
+    }
+
+    return nullptr;
+}
+
+} // namespace pz
---
+++ runtime/pz_gc_collect.cpp
@@ -0,0 +1,210 @@
+/*
+ * Plasma garbage collector collection procedures 
+ * vim: ts=4 sw=4 et
+ *
+ * Copyright (C) 2018-2019 Plasma Team
+ * Distributed under the terms of the MIT license, see ../LICENSE.code
+ */
+
+#include "pz_common.h"
+
+#include <string.h>
+
+#include "pz_util.h"
+
+#include "pz_gc.h"
+#include "pz_gc_util.h"
+
+#include "pz_gc.impl.h"
+#include "pz_gc_layout.h"
+
+namespace pz {
+
+/*
+ * Mask off the low bits so that we can see the real pointer rather than a
+ * tagged pointer.
+ */
+constexpr uintptr_t TAG_BITS = WORDSIZE_BYTES - 1;
+
+constexpr void*
+REMOVE_TAG(void* tagged_ptr) {
+    return reinterpret_cast<void*>(
+            reinterpret_cast<uintptr_t>(tagged_ptr) & (~0 ^ TAG_BITS));
+}
+
+void
+Heap::collect(const AbstractGCTracer *trace_thread_roots)
+{
+    HeapMarkState state(this);
+
+    // There's nothing to collect, the heap is empty.
+    if (is_empty()) return;
+
+#ifdef PZ_DEV
+    assert(!in_no_gc_scope);
+
+    if (m_options.gc_slow_asserts()) {
+        check_heap();
+    }
+#endif
+
+#ifdef PZ_DEV
+    if (m_options.gc_trace()) {
+        fprintf(stderr, "Tracing from global roots\n");
+    }
+#endif
+    m_trace_global_roots.do_trace(&state);
+#ifdef PZ_DEV
+    if (m_options.gc_trace()) {
+        fprintf(stderr, "Done tracing from global roots\n");
+    }
+#endif
+
+#ifdef PZ_DEV
+    if (m_options.gc_trace()) {
+        fprintf(stderr, "Tracing from thread roots (eg stacks)\n");
+    }
+#endif
+    assert(trace_thread_roots);
+    trace_thread_roots->do_trace(&state);
+#ifdef PZ_DEV
+    if (m_options.gc_trace()) {
+        fprintf(stderr, "Done tracing from stack\n");
+    }
+#endif
+
+#ifdef PZ_DEV
+    if (m_options.gc_trace()) {
+        state.print_stats(stderr);
+    }
+#endif
+
+    sweep();
+
+#ifdef PZ_DEV
+    if (m_options.gc_slow_asserts()) {
+        check_heap();
+    }
+#endif
+}
+
+unsigned
+Heap::mark(Cell *cell)
+{
+    unsigned num_marked = 0;
+
+    cell->mark();
+    num_marked++;
+
+    void **ptr = cell->pointer();
+    for (unsigned i = 0; i < cell->size(); i++) {
+        void *cur = REMOVE_TAG(ptr[i]);
+        if (is_valid_cell(cur)) {
+            Cell *field = ptr_to_cell(cur);
+
+            if (field->is_allocated() && !field->is_marked()) {
+                num_marked += mark(field);
+            }
+        }
+    }
+
+    return num_marked;
+}
+
+void
+Heap::sweep()
+{
+    m_bblock->sweep();
+}
+
+void
+BBlock::sweep()
+{
+    for (unsigned i = 0; i < GC_LBLOCK_PER_BBLOCK; i++) {
+        m_blocks[i].sweep();
+    }
+}
+
+void
+LBlock::sweep()
+{
+    if (is_empty()) return;
+
+    for (unsigned i = 0; i < num_cells(); i++) {
+        if (*cell_bits(i) & GC_BITS_MARKED) {
+            // Cell is marked, clear the mark bit, keep the allocated bit.
+            assert(*cell_bits(i) & GC_BITS_MARKED);
+            *cell_bits(i) = GC_BITS_ALLOCATED;
+        } else {
+            // Free the cell.
+            *cell_bits(i) = 0;
+        }
+    }
+}
+
+/***************************************************************************/
+
+void
+HeapMarkState::mark_root(void *heap_ptr)
+{
+    if (heap->is_valid_cell(heap_ptr)) {
+        Cell *cell = heap->ptr_to_cell(heap_ptr);
+
+        if (cell->is_allocated() && !cell->is_marked()) {
+            num_marked += heap->mark(cell);
+            num_roots_marked++;
+        }
+    }
+}
+
+void
+HeapMarkState::mark_root_interior(void *heap_ptr)
+{
+    // This actually makes the pointer aligned to the GC's alignment.  We
+    // should have a different macro for this particular use. (issue #154)
+    heap_ptr = REMOVE_TAG(heap_ptr);
+    if (heap->is_heap_address(heap_ptr)) {
+        while (!heap->is_valid_cell(heap_ptr)) {
+            heap_ptr -= WORDSIZE_BYTES;
+        }
+        // WIP: Maybe we want to calculate the block and call all these
+        // methods on it here.  Then we're not re-calculating it in the
+        // while loop and we can stop searching between blocks.
+        mark_root(heap_ptr);
+    }
+}
+
+void
+HeapMarkState::mark_root_conservative(void *root, size_t len)
+{
+    // Mark from the root objects.
+    for (void **p_cur = (void**)root;
+         p_cur < (void**)(root + len);
+         p_cur++)
+    {
+        mark_root(REMOVE_TAG(*p_cur));
+    }
+}
+
+void
+HeapMarkState::mark_root_conservative_interior(void *root, size_t len)
+{
+    // Mark from the root objects.
+    for (void **p_cur = (void**)root;
+         p_cur < (void**)(root + len);
+         p_cur++)
+    {
+        mark_root_interior(*p_cur);
+    }
+}
+
+void
+HeapMarkState::print_stats(FILE *stream)
+{
+    fprintf(stream,
+            "Marked %d root pointers, marked %u pointers total\n",
+            num_roots_marked,
+            num_marked);
+}
+
+} // namespace pz
