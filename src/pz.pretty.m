1c991d391865959adf7a29bfe49d19fdd1630da9
Paul Bone <paul@bone.id.au>
2019-11-04 14:46:47 +1100

    [compiler/pz] Fix contexts in pz pretty printer
    The PZ pretty printer didn't print contexts in comments correctly.

--- src/pz.pretty.m
+++ src/pz.pretty.m
@@ -140,14 +140,14 @@ pretty_instrs(PZ, Indent, [Instr | Instrs]) =
 pretty_instr_obj(PZ, pzio_instr(Instr)) = pretty_instr(PZ, Instr).
 pretty_instr_obj(_, pzio_context(PZContext)) = Pretty :-
     ( PZContext = pz_context(Context, _),
-        Pretty = singleton(context_string(Context))
+        Pretty = comment ++ singleton(context_string(Context))
     ; PZContext = pz_context_short(Line),
-        Pretty = singleton(":" ++ string(Line))
+        Pretty = comment ++ singleton(":" ++ string(Line))
     ; PZContext = pz_nil_context,
         Pretty = empty
     ).
 pretty_instr_obj(_, pzio_comment(Comment)) =
-    singleton("// ") ++ singleton(Comment).
+    comment ++ singleton(Comment).
 
 :- func pretty_instr(pz, pz_instr) = cord(string).
 
@@ -291,6 +291,10 @@ width_pretty_str(pzw_64)   = "w64".
 % names for these throughout the system.
 width_pretty_str(pzw_fast) = "w".
 width_pretty_str(pzw_ptr)  = "ptr".
+
+:- func comment = cord(string).
+
+comment = singleton("// ").
 
 %-----------------------------------------------------------------------%
 %-----------------------------------------------------------------------%
