31ce7ca17c4cd16c1ebfcb34205647d710bf20d1
Paul Bone <paul@bone.id.au>
2020-09-06 23:31:41 +1000

    [compiler/core] Print type variables with the correct syntax

--- src/core.pretty.m
+++ src/core.pretty.m
@@ -291,7 +291,7 @@ pattern_pretty(Core, Varmap, p_ctor(CtorId, Args)) =
 type_pretty(_, builtin_type(Builtin)) = p_str(Str) :-
     builtin_type_name(Builtin, Name),
     Str = q_name_to_string(q_name_append(builtin_module_name, Name)).
-type_pretty(_, type_variable(Var)) = p_str(Var).
+type_pretty(_, type_variable(Var)) = p_expr([p_str("'"), p_str(Var)]).
 type_pretty(Core, type_ref(TypeId, Args)) =
     pretty_optional_args(
         id_pretty(core_lookup_type_name(Core), TypeId),
