PlasmaLang/plasma

commit d2c8b1b908f0ad3eb385be30d775da282c94d582

[rt] Display the current environment during tracing

runtime/pz_trace.h
@@ -19,8 +19,8 @@ void trace_instr_(unsigned rsp, const char * instr_name);
 
 void trace_instr2_(unsigned rsp, const char * instr_name, int num);
 
-void trace_state_(const Heap * heap, void * ip, unsigned rsp, unsigned esp,
-                  uint64_t * stack);
+void trace_state_(const Heap * heap, void * ip, void * env,
+                  unsigned rsp, unsigned esp, uint64_t * stack);
 
 }  // namespace pz
 
@@ -32,9 +32,9 @@ void trace_state_(const Heap * heap, void * ip, unsigned rsp, unsigned esp,
     if (pz::trace_enabled) {               \
         pz::trace_instr2_(rip, name, num); \
     }
-#define pz_trace_state(heap, rip, rsp, esp, stack)    \
-    if (pz::trace_enabled) {                          \
-        pz::trace_state_(heap, rip, rsp, esp, stack); \
+#define pz_trace_state(heap, rip, env, rsp, esp, stack)    \
+    if (pz::trace_enabled) {                               \
+        pz::trace_state_(heap, rip, env, rsp, esp, stack); \
     }
 
 #else /* ! PZ_DEV */

runtime/pz_generic_run.cpp
@@ -29,6 +29,7 @@ int generic_main_loop(Context & context, Heap * heap, Closure * closure,
 
     pz_trace_state(heap,
                    context.ip,
+                   context.env,
                    context.rsp,
                    context.esp,
                    (uint64_t *)context.expr_stack);
@@ -618,6 +619,7 @@ int generic_main_loop(Context & context, Heap * heap, Closure * closure,
                 pz_trace_instr(context.rsp, "end");
                 pz_trace_state(heap,
                                context.ip,
+                               context.env,
                                context.rsp,
                                context.esp,
                                (uint64_t *)context.expr_stack);
@@ -663,6 +665,7 @@ int generic_main_loop(Context & context, Heap * heap, Closure * closure,
         }
         pz_trace_state(heap,
                        context.ip,
+                       context.env,
                        context.rsp,
                        context.esp,
                        (uint64_t *)context.expr_stack);

runtime/pz_trace.cpp
@@ -36,8 +36,8 @@ void trace_instr2_(unsigned rsp, const char * instr_name, int num)
     fprintf(stderr, "%4u: %s %d\n", rsp, instr_name, num);
 }
 
-void trace_state_(const Heap * heap, void * ip, unsigned rsp, unsigned esp,
-                  uint64_t * stack)
+void trace_state_(const Heap * heap, void * ip, void * env,
+                  unsigned rsp, unsigned esp, uint64_t * stack)
 {
     void * code = heap_interior_ptr_to_ptr(heap, ip);
     assert(ip >= code);
@@ -56,7 +56,7 @@ void trace_state_(const Heap * heap, void * ip, unsigned rsp, unsigned esp,
         builtin = " (builtin)";
     }
 
-    fprintf(stderr, "      IP %p: %s+%ld%s", ip, name, (long)offset, builtin);
+    fprintf(stderr, "      IP  %p: %s+%ld%s", ip, name, (long)offset, builtin);
 
     unsigned line = 0;
     if (proc && proc->filename()) {
@@ -72,6 +72,7 @@ void trace_state_(const Heap * heap, void * ip, unsigned rsp, unsigned esp,
 
     fprintf(stderr, "\n");
 
+    fprintf(stderr, "      ENV %p\n", env);
     fprintf(stderr, "      RSP %4u ESP %4u\n", rsp, esp);
     fprintf(stderr, "      stack: ");
 

commit 2fe1fa82cd5381f280b7eeb4a000b4e3bb9a3cdf

[rt] Fix release builds
Fix the regression in d2c8b1b9

runtime/pz_trace.h
@@ -41,7 +41,7 @@ void trace_state_(const Heap * heap, void * ip, void * env,
 
 #define pz_trace_instr(rip, name)
 #define pz_trace_instr2(rip, name, num)
-#define pz_trace_state(heap, rip, rsp, esp, stack)
+#define pz_trace_state(heap, rip, env, rsp, esp, stack)
 
 #endif /* ! PZ_DEV */
 