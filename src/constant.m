1df78e16992d95a8e9c2579e0eec9a60f6f1b6c8
Paul Bone <paul@bone.id.au>
2021-04-27 00:51:24 +1000

    [compiler] Add file extensions for two new file types
    Add extensions for:
     * Lists of exported types and resources
     * Dependency info for interface files

--- src/constant.m
+++ src/constant.m
@@ -15,7 +15,9 @@
 :- import_module io.
 
 :- func source_extension = string.
+:- func typeres_extension = string.
 :- func interface_extension = string.
+:- func interface_depends_extension = string.
 :- func dep_info_extension = string.
 :- func pz_text_extension = string.
 :- func output_extension = string.
@@ -47,7 +49,9 @@
 %-----------------------------------------------------------------------%
 
 source_extension = ".p".
+typeres_extension = ".typeres".
 interface_extension = ".pi".
+interface_depends_extension = ".pi_dep".
 dep_info_extension = ".dep".
 pz_text_extension = ".pzt".
 output_extension = ".pzo".
