9302804fec89471aa3f0d342be63fea9ebbc85df
Paul Bone <paul@bone.id.au>
2022-09-15 12:56:57 +1000

    [tests] Update update-outputs.sh
    Update the script for updating test outputs.

--- tests-old/update-outputs.sh
+++ tests-old/update-outputs.sh
@@ -6,10 +6,10 @@ set -e
 # change.  The user should check the diffs before committing them.
 
 # Only work in tests directories that incorporate compiler error messages.
-DIRS="tests/invalid 
-      tests/modules-invalid 
-      tests/build-invalid 
-      tests/missing"
+DIRS="tests-old/invalid 
+      tests-old/modules-invalid 
+      tests-old/build-invalid 
+      tests-old/missing"
 
 for TESTDIR in $DIRS; do
     for OUTPUT in $TESTDIR/*.out; do
@@ -27,4 +27,23 @@ for TESTDIR in $DIRS; do
         fi
     done
 done
+
+# Do the same for the new test suite, this will need tweaking as we
+# develop the test suite though.
+TESTDIR=tests/types
+for OUTPUT in $TESTDIR/*.outs; do
+    # If the glob didn't match anything then output won't exist.
+    if [ -e $OUTPUT ]; then
+        BASE=$TESTDIR/`basename $OUTPUT .outs`
+        # Only copy the file if there's already an .exp file.  It's
+        # possible there may be a .out file but no .exp if we've
+        # switched branches recently.  This also has the effect of not
+        # updating .expish files, which is good since those must be
+        # updated manually.
+        if [ -e $BASE.exp ]; then
+            mv $OUTPUT $BASE.exp
+        fi
+    fi
+done
+
 
